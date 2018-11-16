fa3338f3c7d072f5c2312babb22411928f26c2c7
juan david <juandavidlosada2@gmail.com>
2018-11-16 00:10:26 -0500

    Initial commit

---
+++ app/src/main/java/com/imac/whatsapp/Mensaje.java
@@ -0,0 +1,25 @@
+package com.imac.whatsapp;
+
+public class Mensaje {
+
+    private String name;
+    private String mensaje;
+
+    public Mensaje(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+}
